8644bb052e5bd293dce342d52dd98a59faea5538
catherine849 <navyawentcrazy@gmail.com>
2026-02-04 12:56:46 +0530

    Create input.js

---
+++ components/ui/input.js
@@ -0,0 +1,13 @@
+export function Input(props) {
+  return (
+    <input
+      {...props}
+      style={{
+        padding: "10px",
+        borderRadius: "8px",
+        border: "1px solid #ccc",
+        width: "100%"
+      }}
+    />
+  )
+}
