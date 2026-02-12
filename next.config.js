33179e5e6d9f8dc118aa24a8ab5c23a62b5a6afd
catherine849 <navyawentcrazy@gmail.com>
2026-02-12 19:22:39 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-import { SpeedInsights } from "@vercel/speed-insights/next"
-module.exports = nextConfig
+};
+
+module.exports = nextConfig;
